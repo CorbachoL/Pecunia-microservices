f9d0b193949f227cad98a776dc21e6105aa938d0
root <root@DESKTOP-JAC89L7.localdomain>
2022-10-05 21:14:43 +0200

    Pecunia-api-gateway, Pecunia-config y pecunia-discover desarrollados correctamente

--- pecunia-discover/src/main/java/es/corduba/pecunia/pecuniadiscover/EurekaServiceDiscoveryApplication.java
+++ pecunia-discover/src/main/java/es/corduba/pecunia/pecuniadiscover/EurekaServiceDiscoveryApplication.java
@@ -2,12 +2,14 @@ package es.corduba.pecunia.pecuniadiscover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class PecuniaDiscoverApplication {
+@EnableEurekaServer
+public class EurekaServiceDiscoveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PecuniaDiscoverApplication.class, args);
+		SpringApplication.run(EurekaServiceDiscoveryApplication.class, args);
 	}
 
 }
